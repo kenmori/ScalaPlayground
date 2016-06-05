ddcaa2f3eb953306d9256ba966082791925dd2c0
kenji <kennjimoritata@yahoo.co.jp>
2016-06-05 14:06:43 +0900

    add package

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,8 +1,6 @@
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, moria!!!!!")
-    def bark():Unit = println("bow");
-    bark
-    
+    println("Hello, mofafaria!!!!!")
+
   }
 }
